1c2f8b5ff48067b8d23f094bf99dd49d591eabb2
Amr Khaled <lewolfje@gmail.com>
2023-12-12 16:29:13 +0200

    Add file upload feature

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import greetingRoutes from "./features/greeting/greeting.routes.js";
 import goodbyeRoutes from "./features/goodbye/goodbye.routes.js";
+import uploadFileRoutes from "./features/upload-file/uploadFile.routes.js";
 
 const router: Router = Router();
 
 export default (): Router => {
-    greetingRoutes(router);
-    goodbyeRoutes(router);
+  greetingRoutes(router);
+  goodbyeRoutes(router);
+  uploadFileRoutes(router);
 
-    return router;
-}
+  return router;
+};
