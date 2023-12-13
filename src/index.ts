e1daf768dec78d65e5d12c20f74a99820e295d2c
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-12-13 07:36:48 +0200

    Update imports and format

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-
-import helmet from 'helmet';
-import express, { NextFunction, Request, Response } from 'express';
-import compression from 'compression';
+import helmet from "helmet";
+import express, { NextFunction, Request, Response } from "express";
+import compression from "compression";
 import cors from "cors";
+import env, { validateEnvironmentVariables } from "./env.js";
+import router from "./router.js";
 import morgan from "morgan";
 import logger from "core/logger.js";
 
@@ -27,21 +28,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', router());
+app.use("/", router());
 
 // last app.use calls right before app.listen():
 
 // TODO: custom 404
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).send("Sorry can't find that!")
-})
+  res.status(404).send("Sorry can't find that!");
+});
 
 // TODO: custom error handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.listen(env.PORT, () => {
-    console.log('Running on port ' + env.PORT);
+  console.log("Running on port " + env.PORT);
 });
