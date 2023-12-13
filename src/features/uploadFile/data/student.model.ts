f8832ac7ec188cb39c0321f9c05084fcb9d1b4ac
Amr Khaled <lewolfje@gmail.com>
2023-12-13 14:25:30 +0200

    Refactor student model schema

--- src/features/uploadFile/data/student.model.ts
+++ src/features/uploadFile/data/student.model.ts
@@ -1,18 +1,18 @@
-import mongoose, { Schema, Document } from 'mongoose';
-interface IStudentModel extends Document {
+import mongoose, { Schema } from "mongoose";
+type IStudentModel = {
   name: string;
   email: string;
   gpa: number;
   address: string;
 }
 
-const studentSchema: Schema = new Schema({
+const studentSchema: Schema = new Schema<IStudentModel>({
   name: { type: String, required: true },
   email: { type: String, required: true },
   gpa: { type: Number, required: true },
   address: { type: String, required: true },
 });
 
-const StudentModel = mongoose.model<IStudentModel>('Student', studentSchema);
+const StudentModel = mongoose.model<IStudentModel>("Student", studentSchema);
 
 export default StudentModel;
