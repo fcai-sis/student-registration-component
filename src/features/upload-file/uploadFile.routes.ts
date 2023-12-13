4ac984d360d03b526a418ec9fef430605e0c8bf6
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-12-13 06:45:01 +0200

    Remove duplicate feature after rename

--- src/features/upload-file/uploadFile.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import uploadFile from "../upload-file/logic/upload.js";
-import multer from "multer";
-const upload = multer({ dest: "./src/features/upload-file/uploads" });
-export default (router: Router) => {
-  router.post("/upload", upload.single("file"), uploadFile);
-};
