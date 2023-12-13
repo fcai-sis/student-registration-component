83c82ffd6978d9775c3c2f8af0f188015695eca0
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-12-13 08:39:21 +0200

    Add asyncHandler utility function and refactor upload endpoint

--- src/features/uploadFile/logic/upload.endpoint.ts
+++ src/features/uploadFile/logic/upload.endpoint.ts
@@ -3,28 +3,24 @@ import xlsx from "xlsx";
 import fs from "fs";
 import StudentModel from "../data/student.model.js";
 
-export default (req: Request, res: Response) => {
-  try {
-    // Check if the file was uploaded
-    if (!req.file) {
-      res.status(400).send("No file uploaded.");
-      return;
-    }
-
-    // Read the file
-    const buffer = fs.readFileSync(req.file.path);
-
-    // Parse the file
-    const workbook = xlsx.read(buffer, { type: "buffer" });
-
-    // Assuming the first sheet of the Excel file contains the students data
-    const sheetName = workbook.SheetNames[0];
-    const data = xlsx.utils.sheet_to_json<StudentModel>(
-      workbook.Sheets[sheetName]
-    );
-
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send("Error processing the uploaded file.");
+export default async (req: Request, res: Response) => {
+  // Check if the file was uploaded
+  if (!req.file) {
+    res.status(400).send("No file uploaded.");
+    return;
   }
+
+  // Read the file
+  const buffer = fs.readFileSync(req.file.path);
+
+  // Parse the file
+  const workbook = xlsx.read(buffer, { type: "buffer" });
+
+  // Assuming the first sheet of the Excel file contains the students data
+  const sheetName = workbook.SheetNames[0];
+  const data = xlsx.utils.sheet_to_json<StudentModel>(
+    workbook.Sheets[sheetName]
+  );
+
+  res.status(200).send(data);
 };
