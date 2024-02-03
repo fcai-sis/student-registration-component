78a2f4331d2e731d190495f6e3faf9a96b97916d
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-02-03 19:18:12 +0200

    add function to map staged student to mapped student

--- src/features/common/logic/utils/mapping.utils.ts
+++ src/features/common/logic/utils/mapping.utils.ts
@@ -1,3 +1,6 @@
+import logger from "../../../../core/logger";
+import ExcelRow from "../../../registrationSession/data/types/excelRow.type";
+import StudentType from "../../../registrationSession/data/types/student.type";
 import StudentField from "../../../registrationSession/data/types/studentField.type";
 import HasStudentFields from "../../../registrationSession/data/types/hasStudentFields.type";
 
@@ -11,3 +14,22 @@ import HasStudentFields from "../../../registrationSession/data/types/hasStudent
  */
 export const getStudentKeys = (object: HasStudentFields): StudentField[] =>
   Object.keys(object) as StudentField[];
+
+/**
+ * Takes a student row from the Excel file and a mapping and returns a student object.
+ *
+ * @param row A row from the Excel file.
+ * @param mapping A mapping of the Excel columns to the Student fields.
+ * @returns A student object.
+ */
+export const rowToStudent = (row: ExcelRow, mapping: Record<StudentField, string>): StudentType => {
+  logger.debug(`Row ${JSON.stringify(row)}`);
+  logger.debug(`Mapping ${JSON.stringify(mapping)}`);
+
+  const student: Partial<StudentType> = {};
+  Object.entries(mapping).forEach(([field, column]) => {
+    student[field as StudentField] = row[column];
+  });
+
+  return student as StudentType;
+}
