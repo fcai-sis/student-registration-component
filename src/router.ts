c884903e1789912f315973314c2f8763a1b644bc
Amr Khaled <lewolfje@gmail.com>
2024-02-09 20:43:59 +0200

    Add registration session routes to students router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import studentsRoutes from "./features/students/students.routes";
-
+import registrationSessionRoutes from "./features/registrationSession/registrationSession.routes";
 
 export const studentsRouter = (): Router => {
   const router = Router();
   studentsRoutes(router);
+  registrationSessionRoutes(router);
   return router;
 };
