fae84c3a60d0c0e5e985a7f91ddbef0334ecef88
Amr Khaled <lewolfje@gmail.com>
2024-03-20 16:31:08 +0200

    refactor student ID validation

--- src/features/common/data/models/student.model.ts
+++ src/features/common/data/models/student.model.ts
@@ -2,20 +2,15 @@ import mongoose, { Schema } from "mongoose";
 
 import StudentType from "../types/student.type";
 
-/*TODO: check whether the following fields are necessary :
-كود الادارة
-كود الشرطة
-قسم الشرطة*/
-
 const studentSchema: Schema = new Schema<StudentType>({
   studentId: {
     type: String,
     required: [true, "Student ID is required"],
     unique: true,
     validate: {
-      validator: function(value: string) {
-        // studentId must be a string of digits
-        return !/\D/.test(value);
+      validator: function (value: string) {
+        // pattern : string must start with current year and contain only digits
+        return new RegExp(`^${new Date().getFullYear()}\\d{4}$`).test(value);
       },
       message: "Student ID must be a valid ID",
     },
@@ -25,7 +20,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Full name is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // name must contain only Arabic characters and allow whitespace
         return /^[\p{Script=Arabic}\s]+$/gmu.test(value);
       },
@@ -38,7 +33,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: Boolean,
     required: [true, "Group code is required"],
     validate: {
-      validator: function(value: boolean) {
+      validator: function (value: boolean) {
         // Validate if it's a boolean (true or false)
         return typeof value === "boolean";
       },
@@ -50,7 +45,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     enum: ["male", "female", "other"],
     required: [true, "Gender is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         return ["male", "female", "other"].includes(value);
       },
       message: "Gender must be one of the following: male, female, other",
@@ -61,7 +56,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     required: [true, "Religion is required"],
     enum: ["muslim", "christian", "other"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         return ["muslim", "christian", "other"].includes(value);
       },
       message:
@@ -72,7 +67,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "National ID is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // nationalId must be a string of 14 digits
         return /^\d{14}$/.test(value);
       },
@@ -83,7 +78,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Administration is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // administration must contain only letters and Arabic characters and allow whitespace
         return /^[\p{Script=Arabic}\s]+$/gmu.test(value);
       },
@@ -94,7 +89,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Directorate is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // directorate must contain only letters and Arabic characters and allow whitespace
         return /^[\p{Script=Arabic}\s]+$/gmu.test(value);
       },
@@ -105,7 +100,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Phone number is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // phoneNumber must be a string of 11 digits
         return /^\d{11}$/.test(value);
       },
@@ -116,7 +111,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Education type is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // educationType must contain only letters and Arabic characters and allow whitespace
         return /^[\p{Script=Arabic}\s]+$/gmu.test(value);
       },
@@ -127,7 +122,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: Number,
     required: [true, "Birth year is required"],
     validate: {
-      validator: function(value: number) {
+      validator: function (value: number) {
         // birthYear must be a number between 1900 and 2021
         return value >= 1900 && value <= 2021;
       },
@@ -138,7 +133,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: Number,
     required: [true, "Birth month is required"],
     validate: {
-      validator: function(value: number) {
+      validator: function (value: number) {
         // birthMonth must be a number between 1 and 12
         return value >= 1 && value <= 12;
       },
@@ -149,7 +144,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: Number,
     required: [true, "Birth day is required"],
     validate: {
-      validator: function(value: number) {
+      validator: function (value: number) {
         // birthDay must be a number between 1 and 31
         return value >= 1 && value <= 31;
       },
@@ -160,7 +155,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Birth place is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // birthPlace must contain only letters and Arabic characters and allow whitespace
         return /^[\p{Script=Arabic}\s]+$/gmu.test(value);
       },
@@ -171,7 +166,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: Number,
     required: [true, "Governorate ID is required"],
     validate: {
-      validator: function(value: number) {
+      validator: function (value: number) {
         // governorateId must be a number
 
         return !isNaN(value);
@@ -183,7 +178,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Nationality is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         return ["egyptian", "foreigner"].includes(value);
       },
       message: "Nationality must be 'egyptian' or 'foreigner'",
@@ -194,7 +189,7 @@ const studentSchema: Schema = new Schema<StudentType>({
     type: String,
     required: [true, "Address is required"],
     validate: {
-      validator: function(value: string) {
+      validator: function (value: string) {
         // address must contain only letters, numbers and Arabic characters and allow whitespace, and allow / and - characters
         // commented out because ALL THE ADDRESSES ARE ALL OVER THE PLACE
         // return /^[^A-Za-z]+$/gmu.test(value);
