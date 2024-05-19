5ba7a9fcc37886cdcead2a409ed9a160739dd17e
Amr Khaled <lewolfje@gmail.com>
2024-05-19 16:46:49 +0300

    find student by their studentId instead of mongo id

---
+++ src/features/students/logic/handlers/findStudentByStudentId.handler.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { StudentModel } from "@fcai-sis/shared-models";
+
+type HandlerRequest = Request<
+  {
+    studentId: string;
+  },
+  {},
+  {}
+>;
+
+/*
+ * Find student by Id
+ * */
+const handler = async (req: HandlerRequest, res: Response) => {
+  const studentId = req.params.studentId;
+  // read the student from the db
+  const student = await StudentModel.findOne(
+    { studentId },
+    {
+      __v: 0,
+    }
+  );
+
+  if (!student) {
+    return res.status(404).json({
+      error: {
+        message: "Student not found",
+      },
+    });
+  }
+
+  return res.status(200).json({
+    student,
+  });
+};
+
+const findStudentByStudentIdHandler = handler;
+
+export default findStudentByStudentIdHandler;
